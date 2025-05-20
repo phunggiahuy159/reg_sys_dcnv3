257b4605a68e2e547e510b7b5c4dba77012f8e12
phunggiahuy159 <159228989+phunggiahuy159@users.noreply.github.com>
2025-05-20 11:54:19 +0700

    dcnv3->ffm

--- src/DCNv3.py
+++ src/DCNv3.py
@@ -1,5 +1,7 @@
 # =========================================================================
-# Copyright (C) 2024 salmon@github
+# Copyright (C) 2024. The FuxiCTR Library. All rights reserved.
+# Copyright (C) 2022. Huawei Technologies Co., Ltd. All rights reserved.
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -13,190 +15,54 @@
 # limitations under the License.
 # =========================================================================
 
-import torch
 from torch import nn
+import torch
 from fuxictr.pytorch.models import BaseModel
-from fuxictr.pytorch.layers import FeatureEmbedding
-from fuxictr.pytorch.torch_utils import get_regularizer
+from fuxictr.pytorch.layers import FeatureEmbedding, LogisticRegression
 
 
-class DCNv3(BaseModel):
-    def __init__(self,
-                 feature_map,
-                 model_id="DCNv3",
-                 gpu=-1,
-                 learning_rate=1e-3,
-                 embedding_dim=10,
-                 num_deep_cross_layers=4,
-                 num_shallow_cross_layers=4,
-                 deep_net_dropout=0.1,
-                 shallow_net_dropout=0.3,
-                 layer_norm=True,
-                 batch_norm=False,
-                 num_heads=1,
-                 embedding_regularizer=None,
-                 net_regularizer=None,
+class FFM(BaseModel):
+    def __init__(self, 
+                 feature_map, 
+                 model_id="FFM", 
+                 gpu=-1, 
+                 task="binary_classification", 
+                 learning_rate=1e-3, 
+                 embedding_dim=2, 
+                 regularizer=None, 
                  **kwargs):
-        super(DCNv3, self).__init__(feature_map,
-                                    model_id=model_id,
-                                    gpu=gpu,
-                                    embedding_regularizer=embedding_regularizer,
-                                    net_regularizer=net_regularizer,
-                                    **kwargs)
-        self.embedding_layer = MultiHeadFeatureEmbedding(feature_map, embedding_dim * num_heads, num_heads)
-        input_dim = feature_map.sum_emb_out_dim()
-        self.ECN = ExponentialCrossNetwork(input_dim=input_dim,
-                                           num_cross_layers=num_deep_cross_layers,
-                                           net_dropout=deep_net_dropout,
-                                           layer_norm=layer_norm,
-                                           batch_norm=batch_norm,
-                                           num_heads=num_heads)
-        self.LCN = LinearCrossNetwork(input_dim=input_dim,
-                                      num_cross_layers=num_shallow_cross_layers,
-                                      net_dropout=shallow_net_dropout,
-                                      layer_norm=layer_norm,
-                                      batch_norm=batch_norm,
-                                      num_heads=num_heads)
+        super(FFM, self).__init__(feature_map, 
+                                  model_id=model_id, 
+                                  gpu=gpu, 
+                                  embedding_regularizer=regularizer, 
+                                  net_regularizer=regularizer,
+                                  **kwargs)
+        self.num_fields = feature_map.num_fields
+        self.lr_layer = LogisticRegression(feature_map)
+        self.embedding_layers = nn.ModuleList([FeatureEmbedding(feature_map, embedding_dim) 
+                                               for x in range(self.num_fields - 1)]) # (F - 1) x F x bs x dim
         self.compile(kwargs["optimizer"], kwargs["loss"], learning_rate)
         self.reset_parameters()
         self.model_to_device()
 
     def forward(self, inputs):
+        """
+        Inputs: [X, y]
+        """
         X = self.get_inputs(inputs)
-        feature_emb = self.embedding_layer(X)
-        dlogit = self.ECN(feature_emb).mean(dim=1)
-        slogit = self.LCN(feature_emb).mean(dim=1)
-        logit = (dlogit + slogit) * 0.5
-        y_pred = self.output_activation(logit)
-        return_dict = {"y_pred": y_pred,
-                       "y_d": self.output_activation(dlogit),
-                       "y_s": self.output_activation(slogit)}
+        lr_out = self.lr_layer(X)
+        field_wise_emb_list = [each_layer(X) for each_layer in self.embedding_layers] # (F - 1) list of bs x F x d
+        ffm_out = self.ffm_interaction(field_wise_emb_list)
+        y_pred = lr_out + ffm_out
+        y_pred = self.output_activation(y_pred)
+        return_dict = {"y_pred": y_pred}
         return return_dict
 
-    def add_loss(self, inputs):
-        return_dict = self.forward(inputs)
-        y_true = self.get_labels(inputs)
-        y_pred = return_dict["y_pred"]
-        y_d = return_dict["y_d"]
-        y_s = return_dict["y_s"]
-        loss = self.loss_fn(y_pred, y_true, reduction='mean')
-        loss_d = self.loss_fn(y_d, y_true, reduction='mean')
-        loss_s = self.loss_fn(y_s, y_true, reduction='mean')
-        weight_d = loss_d - loss
-        weight_s = loss_s - loss
-        weight_d = torch.where(weight_d > 0, weight_d, torch.zeros(1).to(weight_d.device))
-        weight_s = torch.where(weight_s > 0, weight_s, torch.zeros(1).to(weight_s.device))
-        loss = loss + loss_d * weight_d + loss_s * weight_s
-        return loss
-
-
-class MultiHeadFeatureEmbedding(nn.Module):
-    def __init__(self, feature_map, embedding_dim, num_heads=2):
-        super(MultiHeadFeatureEmbedding, self).__init__()
-        self.num_heads = num_heads
-        self.embedding_layer = FeatureEmbedding(feature_map, embedding_dim)
-
-    def forward(self, X):  # H = num_heads
-        feature_emb = self.embedding_layer(X)  # B × F × D
-        multihead_feature_emb = torch.tensor_split(feature_emb, self.num_heads, dim=-1)
-        multihead_feature_emb = torch.stack(multihead_feature_emb, dim=1)  # B × H × F × D/H
-        multihead_feature_emb1, multihead_feature_emb2 = torch.tensor_split(multihead_feature_emb, 2,
-                                                                            dim=-1)  # B × H × F × D/2H
-        multihead_feature_emb1, multihead_feature_emb2 = multihead_feature_emb1.flatten(start_dim=2), \
-                                                         multihead_feature_emb2.flatten(
-                                                             start_dim=2)  # B × H × FD/2H; B × H × FD/2H
-        multihead_feature_emb = torch.cat([multihead_feature_emb1, multihead_feature_emb2], dim=-1)
-        return multihead_feature_emb  # B × H × FD/H
-
-
-class ExponentialCrossNetwork(nn.Module):
-    def __init__(self,
-                 input_dim,
-                 num_cross_layers=3,
-                 layer_norm=True,
-                 batch_norm=False,
-                 net_dropout=0.1,
-                 num_heads=1):
-        super(ExponentialCrossNetwork, self).__init__()
-        self.num_cross_layers = num_cross_layers
-        self.layer_norm = nn.ModuleList()
-        self.batch_norm = nn.ModuleList()
-        self.dropout = nn.ModuleList()
-        self.w = nn.ModuleList()
-        self.b = nn.ParameterList()
-        for i in range(num_cross_layers):
-            self.w.append(nn.Linear(input_dim, input_dim // 2, bias=False))
-            self.b.append(nn.Parameter(torch.zeros((input_dim,))))
-            if layer_norm:
-                self.layer_norm.append(nn.LayerNorm(input_dim // 2))
-            if batch_norm:
-                self.batch_norm.append(nn.BatchNorm1d(num_heads))
-            if net_dropout > 0:
-                self.dropout.append(nn.Dropout(net_dropout))
-            nn.init.uniform_(self.b[i].data)
-        self.masker = nn.ReLU()
-        self.dfc = nn.Linear(input_dim, 1)
-
-    def forward(self, x):
-        for i in range(self.num_cross_layers):
-            H = self.w[i](x)
-            if len(self.batch_norm) > i:
-                H = self.batch_norm[i](H)
-            if len(self.layer_norm) > i:
-                norm_H = self.layer_norm[i](H)
-                mask = self.masker(norm_H)
-            else:
-                mask = self.masker(H)
-            H = torch.cat([H, H * mask], dim=-1)
-            x = x * (H + self.b[i]) + x
-            if len(self.dropout) > i:
-                x = self.dropout[i](x)
-        logit = self.dfc(x)
-        return logit
-
-
-class LinearCrossNetwork(nn.Module):
-    def __init__(self,
-                 input_dim,
-                 num_cross_layers=3,
-                 layer_norm=True,
-                 batch_norm=True,
-                 net_dropout=0.1,
-                 num_heads=1):
-        super(LinearCrossNetwork, self).__init__()
-        self.num_cross_layers = num_cross_layers
-        self.layer_norm = nn.ModuleList()
-        self.batch_norm = nn.ModuleList()
-        self.dropout = nn.ModuleList()
-        self.w = nn.ModuleList()
-        self.b = nn.ParameterList()
-        for i in range(num_cross_layers):
-            self.w.append(nn.Linear(input_dim, input_dim // 2, bias=False))
-            self.b.append(nn.Parameter(torch.zeros((input_dim,))))
-            if layer_norm:
-                self.layer_norm.append(nn.LayerNorm(input_dim // 2))
-            if batch_norm:
-                self.batch_norm.append(nn.BatchNorm1d(num_heads))
-            if net_dropout > 0:
-                self.dropout.append(nn.Dropout(net_dropout))
-            nn.init.uniform_(self.b[i].data)
-        self.masker = nn.ReLU()
-        self.sfc = nn.Linear(input_dim, 1)
-
-    def forward(self, x):
-        x0 = x
-        for i in range(self.num_cross_layers):
-            H = self.w[i](x)
-            if len(self.batch_norm) > i:
-                H = self.batch_norm[i](H)
-            if len(self.layer_norm) > i:
-                norm_H = self.layer_norm[i](H)
-                mask = self.masker(norm_H)
-            else:
-                mask = self.masker(H)
-            H = torch.cat([H, H * mask], dim=-1)
-            x = x0 * (H + self.b[i]) + x
-            if len(self.dropout) > i:
-                x = self.dropout[i](x)
-        logit = self.sfc(x)
-        return logit
+    def ffm_interaction(self, field_wise_emb_list):
+        dot = 0
+        for i in range(self.num_fields - 1):
+            for j in range(i + 1, self.num_fields):
+                v_ij = field_wise_emb_list[j - 1][:, i, :]
+                v_ji = field_wise_emb_list[i][:, j, :]
+                dot += torch.sum(v_ij * v_ji, dim=1, keepdim=True)
+        return dot
